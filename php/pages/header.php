e19904b5df29b4cee17fa47d09df618d282066f9
yanis chaabane <yanis.chaabane@epita.fr>
2019-04-17 19:05:25 +0200

    Correcting directories

--- php/pages/header.php
+++ php/pages/header.php
@@ -1,16 +1,16 @@
 <div id="logo">
-<img src="../images/logo2.png" width="25%"/>
+<img src="images/logo2.png" width="25%"/>
 </div>
 
 <div id="menu">
-<div class="icon"><a href="../index.php"><img src="../images/icon.png" id="icon"></a></div>
+<div class="icon"><a href="index.php"><img src="images/icon.png" id="icon"></a></div>
 <ul>
-  <li><a href="presentation.php">Présentation</a></li>
-  <li><a href="profils.php">Profils</a></li>
-  <li><a href="programme.php">Calendrier</a></li>
-  <li><a href="problemes.php">Problèmes</a></li>
-  <li><a href="telechargement.php">Téléchargement</a></li>
-  <li><a href="annexes.php">Annexes</a></li>
+  <li><a href="php/pages/presentation.php">Présentation</a></li>
+  <li><a href="php/pages/profils.php">Profils</a></li>
+  <li><a href="php/pages/programme.php">Calendrier</a></li>
+  <li><a href="php/pages/problemes.php">Problèmes</a></li>
+  <li><a href="php/pages/telechargement.php">Téléchargement</a></li>
+  <li><a href="php/pages/annexes.php">Annexes</a></li>
   <!--<li><a href="en/home.html">EN</a></li>-->
 </ul>
 </div>
